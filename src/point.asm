367483a2f4f4d1d44218245fd1b0bf7997660117
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2019-03-16 21:39:24 -0400

    Fix whitespace.

--- src/point.asm
+++ src/point.asm
@@ -11,21 +11,21 @@
 .endmacro
 
 .macro Point_set_x point, newVal
-	lda newVal
-	sta point+Point::x_val
+  lda newVal
+  sta point+Point::x_val
 .endmacro
 
 .macro Point_apply_vector point, vector
-	clv
-	clc
-	lda point+Point::x_val
-	adc vector+Point::x_val
-	sta point+Point::x_val
+  clv
+  clc
+  lda point+Point::x_val
+  adc vector+Point::x_val
+  sta point+Point::x_val
 
-	clv
-	clc
-	lda point+Point::y_val
-	adc vector+Point::y_val
-	sta point+Point::y_val
+  clv
+  clc
+  lda point+Point::y_val
+  adc vector+Point::y_val
+  sta point+Point::y_val
 .endmacro
 
